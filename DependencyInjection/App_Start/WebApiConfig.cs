187b906bc3020abd72814755e29d5d22c7e83c7f
Matt Bimpson <bimpsonm@kssfuels.com>
2017-02-09 11:28:02 +0000

    .

---
+++ DependencyInjection/App_Start/WebApiConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.Owin.Security.OAuth;
+using Newtonsoft.Json.Serialization;
+using Unity.Mvc3;
+using Microsoft.Practices.Unity;
+using System.Web.Mvc;
+
+namespace DependencyInjection
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            // Configure Web API to use only bearer token authentication.
+            config.SuppressDefaultHostAuthentication();
+            config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+
+            // Configure unity
+            IUnityContainer container = UnityContainerManager.Current.GetContainer();
+            //GlobalConfiguration.Configuration.DependencyResolver = new Microsoft.Practices.Unity.WebApi.UnityDependencyResolver(container); // ApiController type
+            DependencyResolver.SetResolver(new UnityDependencyResolver(container)); // Controller type
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
